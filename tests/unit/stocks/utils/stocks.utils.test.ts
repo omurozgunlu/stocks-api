2746c48c0dfe7d031809f97e56492a58fe85be0e
omur <canozgunlu@gmail.com>
2022-06-08 02:01:52 +0300

    end of task 1

--- tests/unit/stocks/utils/stocks.utils.test.ts
+++ tests/unit/stocks/utils/stocks.utils.test.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { query } from "express";
 import stocksUtils from "../../../../stocks/utils/stocks.utils";
 
 describe("util test", () => {
+  let mockRequest: Partial<express.Request>;
+  beforeEach(() => {
+    mockRequest = {};
+  });
   it("returns null when query is empty", () => {
-    let req: express.Request = null;
-    const result = stocksUtils.sanitizeDateQuery(req);
+    const result = stocksUtils.sanitizeDateQuery(
+      mockRequest as express.Request
+    );
+    expect(result).toEqual(null);
+  });
+  it("returns null when date is empty", () => {
+    mockRequest["query"] = {};
+    const result = stocksUtils.sanitizeDateQuery(
+      mockRequest as express.Request
+    );
+    expect(result).toEqual(null);
+  });
+  it("happy path", () => {
+    mockRequest["query"] = { date: "5-January-2000" };
+    const result = stocksUtils.sanitizeDateQuery(
+      mockRequest as express.Request
+    );
+    expect(result).toEqual("5-January-2000");
   });
 });
